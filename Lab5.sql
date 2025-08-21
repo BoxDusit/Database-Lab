7c3a34089e3d4488ce27fc9f871438137b5b187d
_ugboy68 <no09122548@gmail.com>
2025-08-21 14:37:28 +0700

    Update Lab5.sql

--- Lab5.sql
+++ Lab5.sql
@@ -16,3 +16,45 @@ SELECT CompanyName,OrderID
 FROM Orders JOIN Shippers
 ON Shippers.ShipperID=Orders.ShipVia
 WHERE OrderID=10275
+
+SELECT ProductID,CompanyName,Country
+FROM Products Join Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
+WHERE Country in ('usa','uk')
+
+
+SELECT ProductID,ProductName,City,Country
+FROM Products Join Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
+
+SELECT *
+FROM Employees e JOIN Orders o 
+ON e.EmployeeID = o.EmployeeID
+Order By e.EmployeeID
+
+SELECT *
+FROM  Orders O
+JOIN Customers C ON O.CustomerID=C.CustomerID 
+JOIN Employees E ON O.EmployeeID=E.EmployeeID
+
+SELECT s.CompanyName,count(*)จำนวนOrder
+FROM Shippers s JOIN Orders o 
+ON s.ShipperID = o.ShipVia
+GROUP BY s.CompanyName
+ORDER BY 2 desc
+
+SELECT p.ProductID,p.ProductName,sum(Quantity) จำนวนที่ขายได้
+FROM Products p JOIN [Order Details] od 
+ON p.ProductID = od.ProductID
+GROUP BY p.ProductID , p.ProductName
+
+SELECT DISTINCT p.ProductID,p.ProductName
+FROM Employees e 
+JOIN Orders o ON e.EmployeeID = o.EmployeeID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+JOIN Products p ON p.ProductID = od.ProductID
+WHERE e.FirstName = 'Nancy'
+ORDER BY ProductID
+
+
+
