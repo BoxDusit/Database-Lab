99858277df7424f6efe693f5adc07a024799435d
_ugboy68 <no09122548@gmail.com>
2025-08-21 13:53:25 +0700

    Update Lab5.sql

--- Lab5.sql
+++ Lab5.sql
@@ -1,1 +1,18 @@
-//dusit
+SELECT CategoryName,ProductName,UnitPrice
+FROM Products as p,Categories as c
+WHERE p.CategoryID=c.CategoryID
+and CategoryName = 'seafood'
+
+SELECT CategoryName,ProductName,UnitPrice
+FROM Products as p join Categories as c
+ON p.CategoryID=c.CategoryID
+WHERE CategoryName = 'seafood'
+
+SELECT CompanyName,OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID = Orders.ShipVia
+
+SELECT CompanyName,OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.ShipVia
+WHERE OrderID=10275
